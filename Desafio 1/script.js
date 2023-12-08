f685a1c687f1c1dbe0c7dbd0117f01bb31e60102
Grazi 1988 <grazi_510@hotmail.com>
2023-12-08 15:12:48 -0300

    Desafio

---
+++ Desafio 1/script.js
@@ -0,0 +1,26 @@
+console.log ("Ano novo está chegando!!");
+
+let nome = "Mariah Carey";
+let idade = "54";
+let ano = 2023 ;
+
+const anoNascimento= ano-idade;
+console.log (anoNascimento);
+
+let quartaFeira = (true);
+console.log (quartaFeira);
+
+let _idade = 18
+if (_idade >= 18){
+console.log ("Você é maior de idade, poderá curtir o show do Roberto Carlos com participação especial de Mariah Carey.")
+}else{
+console.log ("Sinto muito, mas assistirá da tv globo.")
+}
+
+let niver =`Outubro`;
+
+if (niver == `Agosto` || niver == `Dezembro` || niver == `Junho`){
+    console.log (`Uma ou mais opções estão incorretas`)
+}else{
+    console.log (`Algo de errado não está certo, o mês digitado foi ${niver}`)
+}
